1d9ded662425d9abf92a74c4bc4930eabaab49bf
Rohan Creasey <rcreasey@rubiconproject.com>
2016-07-13 22:13:38 +1000

    Update main.js

--- main.js
+++ main.js
@@ -11,33 +11,33 @@
   var header = document.querySelector('header') // header 
   var state = {
        newsSources : [
-	        {
-            name: 'Reddit',
-            url: 'https://www.reddit.com/top.json'      
-          },
-          {name: 'newsSource1rohan'},
-          {name: 'newsSource2amy'},
-          {test: 'testValue'}       
-        ],    // array of article sources
-  }
+       			{
+				name: 'Reddit',
+				url: 'https://www.reddit.com/top.json'      
+			},
+			{name: 'newsSource1rohan'},
+			{name: 'newsSource2amy'},
+			{test: 'testValue'}       
+		],    // array of article sources
+	}
 
 
 // renderLoading call for loading pacman
-  function renderLoading(data, into) {
+function renderLoading(data, into) {
     // TODO: Add the template
-      into.innerHTML = `        
-        <div id="pop-up" class="loader">
-          </div>
-      `
-  }
+	into.innerHTML = `        
+		<div id="pop-up" class="loader">
+		</div>
+	`
+}
 
 
 function renderNavItem(item) {
-      return `
-        <a href="${item.name}">
-            ${item.name}
-        </a>
-  	   `
+	return `
+		<a href="${item.name}">
+			${item.name}
+		</a>
+	`
 }
 
 /* renderNav function to show nav bar
@@ -45,89 +45,89 @@ function renderNavItem(item) {
    with results of calling renderSource() on every element in state.newsSources array
 */
 function renderNav(state, into) {
-    into.innerHTML = `
-    <section class="wrapper">
-      <a href="#"><h1>Feedr</h1></a>
-      <nav>
-        <section id="search">
-          <input type="text" name="name" value="">
-          <div id="search-icon"><img src="images/search.png" alt="" /></div>
-        </section>
-        <ul>
-          <li><a href="#">News Source: <span>Source Name</span></a>
-          <ul>
-            ${state.newsSources.map((item) => {
-                return `<li>${renderNavItem(item)}</li>`
-            }).join('')}
-          </ul>
-          </li>
-        </ul>
-      </nav>
-      <div class="clearfix"></div>
-    </section>
-    `
+	into.innerHTML = `
+	<section class="wrapper">
+		<a href="#"><h1>Feedr</h1></a>
+			<nav>
+				<section id="search">
+					<input type="text" name="name" value="">
+					<div id="search-icon"><img src="images/search.png" alt="" /></div>
+				</section>
+				<ul>
+					<li><a href="#">News Source: <span>Source Name</span></a>
+				<ul>
+					${state.newsSources.map((item) => {
+						return `<li>${renderNavItem(item)}</li>`
+					}).join('')}
+				</ul>
+					</li>
+				</ul>
+			</nav>
+		<div class="clearfix"></div>
+	</section>
+	`
 }
 
 
 // renderArticleList() to show list of articles, inside #container
 function renderArticleList(state, into) {
-  into.innerHTML = `
-     <section id="main" class="wrapper">
-     MOO
-      <article class="article">
-        <section class="featured-image">
-          <img src="images/article_placeholder_1.jpg" alt="" />
-        </section>
-        <section class="article-content">
-          <a href="#"><h3>Test article title</h3></a>
-          <h6>Lifestyle</h6>
-        </section>
-        <section class="impressions">
-          526
-        </section>
-        <div class="clearfix"></div>
-      </article>
-      <article class="article">
-        <section class="featured-image">
-          <img src="images/article_placeholder_2.jpg" alt="" />
-        </section>
-        <section class="article-content">
-          <a href="#"><h3>Test article title</h3></a>
-          <h6>Lifestyle</h6>
-        </section>
-        <section class="impressions">
-          526
-        </section>
-        <div class="clearfix"></div>
-      </article>
-      <article class="article">
-        <section class="featured-image">
-          <img src="images/article_placeholder_2.jpg" alt="" />
-        </section>
-        <section class="article-content">
-          <a href="#"><h3>Test article title</h3></a>
-          <h6>Lifestyle</h6>
-        </section>
-        <section class="impressions">
-          526
-        </section>
-        <div class="clearfix"></div>
-      </article>
-      <article class="article">
-        <section class="featured-image">
-          <img src="images/article_placeholder_1.jpg" alt="" />
-        </section>
-        <section class="article-content">
-          <a href="#"><h3>Test article title</h3></a>
-          <h6>Lifestyle</h6>
-        </section>
-        <section class="impressions">
-          526
-        </section>
-        <div class="clearfix"></div>
-      </article>
-    </section>  
-  `
+	into.innerHTML = `
+		<section id="main" class="wrapper">
+			MOO
+			<article class="article">
+				<section class="featured-image">
+					<img src="images/article_placeholder_1.jpg" alt="" />
+				</section>
+				<section class="article-content">
+					<a href="#"><h3>Test article title</h3></a>
+					<h6>Lifestyle</h6>
+				</section>
+				<section class="impressions">
+					526
+				</section>
+				<div class="clearfix"></div>
+			</article>
+			<article class="article">
+				<section class="featured-image">
+					<img src="images/article_placeholder_2.jpg" alt="" />
+				</section>
+				<section class="article-content">
+					<a href="#"><h3>Test article title</h3></a>
+					<h6>Lifestyle</h6>
+				</section>
+				<section class="impressions">
+					526
+				</section>
+				<div class="clearfix"></div>
+			</article>
+			<article class="article">
+				<section class="featured-image">
+					<img src="images/article_placeholder_2.jpg" alt="" />
+				</section>
+				<section class="article-content">
+					<a href="#"><h3>Test article title</h3></a>
+					<h6>Lifestyle</h6>
+				</section>
+				<section class="impressions">
+					526
+				</section>
+				<div class="clearfix"></div>
+			</article>
+			<article class="article">
+				<section class="featured-image">
+				          <img src="images/article_placeholder_1.jpg" alt="" />
+				</section>
+				<section class="article-content">
+					<a href="#"><h3>Test article title</h3></a>
+					<h6>Lifestyle</h6>
+				</section>
+				<section class="impressions">
+					526
+				</section>
+				<div class="clearfix"></div>
+			</article>
+		</section>  
+	`
 }
 
 
@@ -148,13 +148,13 @@ function renderArticleList(state, into) {
 // create a function for fetch action as want to repeat it
 
 function fetchPosts(callback){
-		fetch(callback.url)   // fetch .url object of callback parameter
-      .then(function(response) {
-			  return response.json()
+	fetch(callback.url)   // fetch .url object of callback parameter
+		.then(function(response) {
+			return response.json()
 		}).then(function(dataAsJson) {
-			  console.log(dataAsJson);
-		})
-	}
+			console.log(dataAsJson);
+	})
+}
 
 fetchPosts(state.newsSources[]); // argument becomes the parameter callback in line 150
                                   // so should reference newsSources array
